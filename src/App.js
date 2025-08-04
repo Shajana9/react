d3b0298b2fb651c80dea18a18b4731390cf254a9
sajee lakc <sajeelakc@gmail.com>
2025-08-04 08:48:18 +0000

    init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
+  const [advice, setAdvice] = useState("");
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+  }
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <button>get advice</button>
+      <h1>{advice}</h1>
+      <button onClick={getAdvice}>get advice</button>
+      <p>
+        you have read <strong>23</strong> piece of advice
+        </p>
     </div>
   );
 }
